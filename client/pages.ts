fe2cb6f7a748ad2fadb075bea8733f4002c1e8f1
osawa-koki <github@osawa.dev>
2023-07-24 15:46:11 +0900

    不要なファイルを削除した。

--- client/pages.ts
+++ client/pages.ts
@@ -3,16 +3,6 @@ const pages = [
     emoji: '🏠',
     path: '/',
     name: 'Home'
-  },
-  {
-    emoji: '📖',
-    path: '/about/',
-    name: 'About'
-  },
-  {
-    emoji: '💓',
-    path: '/contact/',
-    name: 'Contact'
   }
 ]
 
